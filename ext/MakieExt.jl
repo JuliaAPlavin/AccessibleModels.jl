f05ce97e93b207a7cc9f1e2cf06ad72337b987cb
Alexander Plavin <alexander@plav.in>
2025-07-01 00:00:00 +0000

    sliders start with initial values

--- ext/MakieExt.jl
+++ ext/MakieExt.jl
@@ -1,20 +1,23 @@
 module MakieExt
 
 using AccessibleModels
-using AccessibleModels: from_transformed
+using AccessibleModels: from_transformed, transformed_vec
 using AccessibleModels.Printf
 using AccessibleModels.DataManipulation
 using Makie
 
 function Makie.SliderGrid(pos, m::AccessibleModel; fmt=x -> @sprintf("%.3f", x), kwargs...)
     result = Observable{Any}(m.modelobj)
+    tvec = transformed_vec(m)
+    i_tvec = 0
 	sliders = flatmap(enumerate(m.optics)) do (i, o)
         curvals = @lift getall($result, o)
         N = AccessorsExtra.nvals_optic(m.modelobj, o)
         map(1:N) do j
             Label(pos[i,1][j,1], "$(AccessorsExtra.barebones_string(o)) #$j")
             Label(pos[i,1][j,3], @lift fmt($curvals[j]))
-            sl = Slider(pos[i,1][j,2]; range=range(0,1; length=300), startvalue=0.5, kwargs...)
+            i_tvec += 1
+            sl = Slider(pos[i,1][j,2]; range=range(0,1; length=300), startvalue=tvec[i_tvec], kwargs...)
         end
 	end
 	Label(pos[0,:], "$(nameof(typeof(m.modelobj))):", tellwidth=false)
