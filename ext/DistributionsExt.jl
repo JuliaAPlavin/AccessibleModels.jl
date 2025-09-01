23cbbc9121b72fa1a5ad615b979fd5be58fc69a4
Alexander Plavin <alexander@plav.in>
2025-09-01 00:00:00 +0000

    allow pigeons with just intervals specified

--- ext/DistributionsExt.jl
+++ ext/DistributionsExt.jl
@@ -3,10 +3,12 @@ module DistributionsExt
 using AccessibleModels
 using Distributions
 
-# Distribution-related methods
-AccessibleModels._product_distribution(distributions...) = product_distribution(distributions...)
+AccessibleModels._product_distribution(distributions...) = product_distribution(map(_to_distribution, distributions)...)
 AccessibleModels._cdf(d::UnivariateDistribution, x) = cdf(d, x)
 AccessibleModels._quantile(d::UnivariateDistribution, p) = quantile(d, p)
 AccessibleModels._logpdf(d, x) = logpdf(d, x)
+
+_to_distribution(d::Distribution) = d
+_to_distribution(d) = Uniform(extrema(d)...)  # XXX: for intervals only, should create DistributionsIntervalsExt?
 
 end
