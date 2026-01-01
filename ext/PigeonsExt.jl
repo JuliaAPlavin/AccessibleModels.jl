410518e5f284827b6ce7b00b7246306b0303fe40
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    Optimization works

---
+++ ext/PigeonsExt.jl
@@ -0,0 +1,9 @@
+module PigeonsExt
+
+using AccessibleModels
+using Pigeons
+
+# Extension methods for Pigeons integration with AccessibleModels
+# Add your Pigeons-specific functionality here
+
+end
